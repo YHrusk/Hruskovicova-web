4da350401d6084bb6ad4e3bf90daa68d31e5cd9e
YHrusk <hruskovicova.i@gmail.com>
2022-04-07 19:10:57 +0200

    server

--- server/routes/articles.js
+++ server/routes/articles.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const articles = [
+/*let articles = [
     {
         id: 1,
         image: 'https://picsum.photos/id/12/200',
@@ -61,9 +61,17 @@ const articles = [
 `
     }
 ]
+*/
+
+const options = {
+    verbose: console.debug
+};
+
+const db = require('better-sqlite3')('articles.sqlite', options);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+    const articles = db.prepare('SELECT * FROM article').all();
     res.send(articles);
 });
 
@@ -74,6 +82,49 @@ router.get('/:id', function(req, res, next){
         res.send(article);
     }else{
         res.send("Article not found");
+    }
+});
+
+router.post("/", function(req,res, next) {
+    const body = req.body;
+    console.log(body);
+    const article = {
+        image: body.image,
+        title: body.title,
+        date: new Date().toISOString(),
+        text: body.text
+    }
+    const stm = db.prepare("INSERT INTO article (image, title, date, text) VALUES (?,?,?,?)");
+    stm.run(...Object.values(article))
+    res.send(article);
+});
+
+router.patch("/:id", function(req,res, next) {
+    const body = req.body;
+    const id = req.params.id;
+    if(id){
+        const article = db.prepare('SELECT * FROM article WHERE id = ?').get(id);
+        if(article){
+            Object.assign(article, body);
+            const stm = db.prepare(
+                "UPDATE article SET image = ?, title = ?, date = ?, text = ? WHERE id = ?"
+            );
+            stm.run(article.image, article.title,article.date,article.text,parseInt(id));
+        } else{
+            res.sendStatus(404);
+        }
+    }else{
+        res.sendStatus(404);
+    }
+});
+
+router.delete("/:id", function(req,res, next) {
+    const id = req.params.id;
+    if(id){
+        db.prepare("DELETE FROM article WHERE id = ?").run(id)
+        res.sendStatus(200);
+    }else{
+        res.sendStatus(404);
     }
 });
 
